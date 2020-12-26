a84a47250ba346d1ce87c0635d84b8f1d9fb68a1
Justin Clareburt <justinclareburt@gmail.com>
2020-12-26 21:35:54 +1000

    Added package registration (Help->About)

--- HotStatus/HotStatusPackage.cs
+++ HotStatus/HotStatusPackage.cs
@@ -7,5 +7,9 @@ namespace HotStatus
     /// The Package will not be loaded into VS unless referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    // InstalledProductRegistration is the information that appears in Help->About (Name, Description, Version)
+    [InstalledProductRegistration("HotStatus Extension",
+        "HotStatus Visual Studio Extension by Justin Clareburt. Shows error messages (and warning and info) on the status bar when the caret (keyboard cursor) is on an error marker (ie. squiggle)", 
+        "1.0.2", IconResourceID = 400)]
     public sealed class HotStatusPackage : AsyncPackage { }
 }
